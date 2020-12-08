6ddc9a0cb8595fbef48bb7d40612dc24576fb9ad
victorzambrano <ingenieriainversazambrano@gmail.com>
2020-12-08 01:00:24 -0400

    actividad23.2

---
+++ src/pages/home/components/introduccion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Introduccion = ()=>{
+     return(
+          <div>
+               <h1>Hola  Profesor abdel tarea 23.2 culminada.</h1>
+          </div>
+     )
+}
+
+export default Introduccion;
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Introduccion from './components/introduccion.js'
+
+export default class Home extends Component {
+     render(){
+          return (
+               <div>
+                    <Introduccion/>
+               </div>
+          )
+     }
+}
