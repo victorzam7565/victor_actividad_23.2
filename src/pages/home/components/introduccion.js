3c763906d937a06ebcdedd3a2208d362d4cc4d8e
victorzambrano <ingenieriainversazambrano@gmail.com>
2020-12-10 16:28:25 -0400

    ahora prueba

--- src/pages/home/components/introduccion.js
+++ src/pages/home/components/introduccion.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Introduccion = ()=>{
      return(
           <div>
-               <h1>Hola Profesor abdel tarea 23.2 culminada..</h1>
+               <h1>Hola Profesor abdel tarea 23.2 culminada...</h1>
           </div>
      )
 }
